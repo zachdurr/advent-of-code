55394cc698d9a100cfadcd452b0b0a8c80822305
zachdurr <zach@remo.health>
2024-12-03 13:57:50 -0500

    day 3

--- 2024/main.ts
+++ 2024/main.ts
@@ -1,8 +1,53 @@
-export function add(a: number, b: number): number {
-  return a + b;
-}
+import { readFile } from 'node:fs/promises';
 
-// Learn more at https://docs.deno.com/runtime/manual/examples/module_metadata#concepts
-if (import.meta.main) {
-  console.log("Add 2 + 3 =", add(2, 3));
-}
+const day3 = async () => {
+	try {
+		const parseAndMultiply = (mulString: string): number => {
+			const numbers = mulString.split('(')[1].split(',');
+			const firstNumber = parseInt(numbers[0]);
+			const secondNumber = parseInt(numbers[1].split(')')[0]);
+			const product = firstNumber * secondNumber;
+			return product;
+		};
+
+		const getSumOfMuls = (dataString: string): number => {
+			const mulRegEx = new RegExp(/mul\(\d{1,3},\d{1,3}\)/g);
+			const matches = dataString.match(mulRegEx);
+			let sum = 0;
+			matches?.forEach((match) => {
+				sum += parseAndMultiply(match);
+			});
+
+			return sum;
+		};
+
+		const getSumOfEnabledMuls = (dataString: string) => {
+			let sum = 0;
+			const doDontRegEx = /mul\(\d*,\d*\)|don\'t\(\)|do\(\)/g;
+			const validMatches = [...dataString.matchAll(doDontRegEx)].map((match) => match[0]);
+			let disabled = false;
+			validMatches.forEach((match) => {
+				if (match === 'do()') {
+					disabled = false;
+				} else if (match === "don't()") {
+					disabled = true;
+				} else if (!disabled) {
+					sum += parseAndMultiply(match);
+				}
+			});
+
+			return sum;
+		};
+
+		await readFile('./day3/input.txt').then((data) => {
+			const rawDataString = data.toString();
+
+			console.log('part 1: ', getSumOfMuls(rawDataString));
+			console.log('part 2: ', getSumOfEnabledMuls(rawDataString));
+		});
+	} catch (err) {
+		console.error(err);
+	}
+};
+
+day3();
